fdd1fdc2ed52c80be34dab6d70e6ea24e57f593a
Naveen.S <naveensagayaselvaraj@gmail.com>
2019-12-31 21:24:13 +0530

    after release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
   name = 'ctreport_selenium',
-  version = '0.1',
+  version = '0.4',
   author='Naveen.S',
   author_email='naveensagayaselvaraj@gmail.com',
   license='MIT',
