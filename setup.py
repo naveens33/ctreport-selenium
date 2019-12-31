50b29b61da1cead20b6029fb441a6529dc2bdb76
Naveen.S <naveensagayaselvaraj@gmail.com>
2019-12-31 20:50:06 +0530

    after release

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
+import setuptools
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
 setup(
-  name = 'ctreport-selenium',         # How you named your package folder (MyLib)
-  packages = ['ctreport-selenium'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'ctreport for selenium automation will provide simple and creative html file for day to day software testing',   # Give a short description about your library
-  author = 'Naveen.S',                   # Type in your name
-  author_email = 'naveensagayaselvaraj@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/naveens33/ctreport-selenium',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/naveens33/ctreport-selenium/archive/0.1.tar.gz',    # I explain this later on
-  keywords = ['Report', 'Selenium', 'CTReport', 'result', 'testing'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'ctreport_selenium',
+  version = '0.1',
+  author='Naveen.S',
+  author_email='naveensagayaselvaraj@gmail.com',
+  license='MIT',
+  description = 'ctreport for selenium automation will provide simple and creative html file for day to day software testing',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  packages=setuptools.find_packages(),
+  url = 'https://github.com/naveens33/ctreport-selenium',
+  download_url = 'https://github.com/naveens33/ctreport-selenium/archive/0.1.tar.gz',
+  install_requires=[
           'datetime',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
   ],
+  python_requires='>=3.6',
 )
